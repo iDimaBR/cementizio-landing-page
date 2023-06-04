ec0c78f8ca1f4b9b5a04030b5242cd24dec95fe9
Raphael Alexei <raphaeeelbr@gmail.com>
2023-06-04 13:02:16 -0300

    Carrosel arrumado

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,21 +1,30 @@
+let slidesPerView = [1.5, 2.5, 3.5];
 
 const swiper1 = new Swiper('.swiper', {
-    // Optional parameters
-    slidesPerView: 2,
-    slidesPerGroup: 3,
-    direction: 'horizontal',
-    spaceBetween: -400,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      dynamicMainBullets: true,
+  // Optional parameters
+  slidesPerView: slidesPerView[0],
+  direction: 'horizontal',
+  spaceBetween: -400,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    dynamicMainBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: slidesPerView[1]
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    1200: {
+      slidesPerView: slidesPerView[2]
     }
+  }
 });
